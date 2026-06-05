05dabfcfd04714a8bf75595f0499615cd6583ce2
gq97a6 <sz.kg@protonmail.com>
2026-06-05 19:50:13 +0200

    Changed experiments table.

--- iso-host/config/includes.chroot/root/benchmark/generate.py
+++ iso-host/config/includes.chroot/root/benchmark/generate.py
@@ -22,14 +22,23 @@ def map_experiments(experiments: list[Experiment], repeat_count: int) -> list[Ex
     random.shuffle(exps)
     return exps
 
+
+@dataclass
+class Experiment:
+    vpn: str = BASELINE["vpn"]
+    delay: str = BASELINE["delay"]
+    jitter: str = BASELINE["jitter"]
+    loss: str = BASELINE["loss"]
+    cpu_freq: str = BASELINE["cpu_freq"]
+    core_count: int = BASELINE["core_count"]
+    
 flat_experiments = [
         Experiment(), # Baseline 1
+        Experiment(delay="40ms", jitter="10ms", loss="0.5%"), # Realistic
         Experiment(delay="50ms"), # Cross-country / inter-state
         Experiment(delay="100ms"), # Transatlantic
         Experiment(delay="300ms"), # Satellite / terrible LTE
-        Experiment(delay="100ms", jitter="20ms"), # Normal wireless variance
         Experiment(delay="100ms", jitter="80ms"), # Extreme out-of-order delivery
-        Experiment(loss="0.1%"), # Bad cable
         Experiment(loss="1.0%"), # Overloaded neighborhood node
         Experiment(loss="3.0%"), # Virtually dead for TCP bulk transfer
         Experiment(core_count=1, cpu_freq="2.0GHz"), # The Low-End Box 1
